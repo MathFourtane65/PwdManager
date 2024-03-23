a48a2f6b6ed9eade5bc7a3138d0c3642361f185e
Mathieu Fourtané <mathieu.fourtane@gmail.com>
2024-03-23 20:10:32 +0100

    open modal when click on item list in homePage + start copy clipboard function

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, Text, TextInput, TouchableOpacity, Alert, ToastAndroid } from 'react-native';
-import { AntDesign } from '@expo/vector-icons'; // Assurez-vous d'avoir installé @expo/vector-icons
+import { StyleSheet, View, FlatList, Text, TextInput, TouchableOpacity, Alert, ToastAndroid, Modal, Platform } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import { useFocusEffect } from '@react-navigation/native';
 import { getPasswords, deletePassword } from '../services/PasswordService';
 import { useTranslation } from 'react-i18next';
+import * as Clipboard from 'expo-clipboard';
 
 const HomeScreen = () => {
   const { t, i18n } = useTranslation();
   const [passwords, setPasswords] = useState([]);
-  const [visiblePasswords, setVisiblePasswords] = useState({}); // Nouvel état pour le suivi de la visibilité
+  const [visiblePasswords, setVisiblePasswords] = useState({});
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedPassword, setSelectedPassword] = useState(null);
 
   useFocusEffect(
     React.useCallback(() => {
@@ -50,20 +54,33 @@ const HomeScreen = () => {
           onPress: () => console.log("Suppression annulée"),
           style: "cancel"
         },
-        { 
+        {
           text: "Oui", onPress: async () => {
             console.log('Suppression validée');
             deletePassword(passwordId);
             setPasswords(await getPasswords());
             ToastAndroid.showWithGravity('Mot de passe supprimé !', ToastAndroid.SHORT, ToastAndroid.TOP)
 
-          } 
+          }
         }
       ],
       { cancelable: false }
     );
   };
-  
+
+  const handlePress = (item) => {
+    setSelectedPassword(item);
+    setModalVisible(true);
+  };
+
+  const copyToClipboard = (text) => {
+    Clipboard.setStringAsync(text);
+    if (Platform.OS === 'android') {
+      ToastAndroid.show("Copied to clipboard!", ToastAndroid.SHORT);
+    } else {
+      Alert.alert('Copied', 'Copied to clipboard!');
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -72,31 +89,64 @@ const HomeScreen = () => {
         data={passwords}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <TouchableOpacity onLongPress={() => handleLongPress(item.id)}>
-          <View style={styles.card}>
-            <View style={styles.cardHeader}>
-              <Text style={styles.cardTitle}>{item.service}</Text>
+          <TouchableOpacity onPress={() => handlePress(item)} onLongPress={() => handleLongPress(item.id)}>
+            <View style={styles.card}>
+              <View style={styles.cardHeader}>
+                <Text style={styles.cardTitle}>{item.service}</Text>
+              </View>
+              <Text>{item.user}</Text>
+              <View style={styles.cardBody}>
+                <TextInput
+                  style={styles.input}
+                  secureTextEntry={!visiblePasswords[item.id]}
+                  value={item.password}
+                  editable={false}
+                />
+                <TouchableOpacity
+                  style={styles.toggle}
+                  onPress={() => togglePasswordVisibility(item.id)}
+                >
+                  <AntDesign name={visiblePasswords[item.id] ? 'eye' : 'eyeo'} size={24} />
+                </TouchableOpacity>
+              </View>
             </View>
-            <Text><Text style={styles.label}>{t('usernameLabelHomeScreen')} :</Text> {item.user}</Text>
-            <View style={styles.cardBody}>
-              <Text>{t('passwordLabelHomeScreen')} : </Text>
-              <TextInput 
-                style={styles.input}
-                secureTextEntry={!visiblePasswords[item.id]} 
-                value={item.password} 
-                editable={false}
-              />
-              <TouchableOpacity
-                style={styles.toggle}
-                onPress={() => togglePasswordVisibility(item.id)}
-              >
-                <AntDesign name={visiblePasswords[item.id] ? 'eye' : 'eyeo'} size={24} />
-              </TouchableOpacity>
-            </View>
-          </View>
           </TouchableOpacity>
         )}
       />
+
+<Modal
+  animationType="fade"
+  transparent={true}
+  visible={modalVisible}
+  onRequestClose={() => setModalVisible(!modalVisible)}
+>
+  <View style={styles.centeredModalView}>
+    <View style={styles.modalContent}>
+      <TouchableOpacity
+        style={styles.closeButton}
+        onPress={() => setModalVisible(!modalVisible)}
+      >
+        <AntDesign name="close" size={24} color="black" />
+      </TouchableOpacity>
+
+      <Text style={styles.modalText}>{selectedPassword?.user}</Text>
+      {/* <TouchableOpacity onPress={() => copyToClipboard(selectedPassword?.user)}>
+        <AntDesign name="copy1" size={24} color="black" />
+      </TouchableOpacity> */}
+
+      <Text style={styles.modalText}>{selectedPassword?.password}</Text>
+      {/* <TouchableOpacity onPress={() => copyToClipboard(selectedPassword?.password)}>
+        <AntDesign name="copy1" size={24} color="black" />
+      </TouchableOpacity> */}
+
+      {/* <TouchableOpacity onPress={() => copyToClipboard(selectedPassword?.user)}>
+        <Text>Copier Username<AntDesign name="copy1" size={24} color="black" /></Text>
+      </TouchableOpacity> */}
+
+    </View>
+  </View>
+</Modal>
+
     </View>
     // <View style={styles.container}>
     //   <Text style={styles.title}>{t('homeScreenTitle')}</Text>
@@ -154,12 +204,12 @@ const styles = StyleSheet.create({
   toggle: {
     marginLeft: 10,
   },
-  title:{
+  title: {
     fontSize: 25,
     textAlign: 'center',
     margin: 10,
     color: '#8A7967',
-    fontWeight:'500'
+    fontWeight: '500'
   },
   card: {
     backgroundColor: 'white',
@@ -178,7 +228,7 @@ const styles = StyleSheet.create({
   },
   cardTitle: {
     fontSize: 18,
-    fontWeight:'500',
+    fontWeight: '500',
     fontStyle: 'italic'
   },
   cardBody: {
@@ -191,7 +241,37 @@ const styles = StyleSheet.create({
     color: '#000000',
   },
   label: {
-  textDecorationLine: 'underline'
+    textDecorationLine: 'underline'
+  },
+  centeredModalView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fond semi-transparent
+  },
+  modalContent: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  closeButton: {
+    position: "absolute",
+    right: 10,
+    top: 10,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
   // ... autres styles
 });
